74bc321baf7cc6768c51c1e0af976026ceac28dd
sohag-aust <frustratedcoder96@gmail.com>
2026-02-11 17:59:50 +0600

    negative test case

--- src/test/java/com/solvians/showcase/CertificateUpdateGeneratorTest.java
+++ src/test/java/com/solvians/showcase/CertificateUpdateGeneratorTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CertificateUpdateGeneratorTest {
 
@@ -21,9 +20,27 @@ class CertificateUpdateGeneratorTest {
     @Test
     public void generateISIN() {
         ISINGenerator isinGenerator = new ISINGenerator();
-        isinGenerator.generateISINList();
+        List<String> isinList = isinGenerator.generateISINList();
 
         String checkDigit = isinGenerator.getCheckDigit("DE123456789");
         assertEquals("6", checkDigit);
+
+        // try to change check number for checking with checkDigit
+        String isinAt3 = isinList.get(2);
+        System.out.println("isinAt3 : " + isinAt3); // CDMSXkgHRWB6
+
+        String temp = isinAt3.substring(0, isinAt3.length()-1);
+//        System.out.println("temp at 3 : " + temp);
+
+        String first = temp.substring(0,3);
+        String temper = "Y";
+        String second = temp.substring(4, temp.length());
+
+        String temperedCheckNumber = first.concat(temper).concat(second);
+        System.out.println("temperedCheckNumber : " + temperedCheckNumber);
+
+        // checking negative case
+        String checkNegativeDigit = isinGenerator.getCheckDigit(isinAt3);
+        assertNotEquals("9", checkNegativeDigit);
     }
 }
